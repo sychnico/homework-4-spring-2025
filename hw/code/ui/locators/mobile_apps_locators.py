c8a6f62045111f92cc104b5335c49db9eb7ebc09
Sonya <sonyabely@gmail.com>
2025-05-17 20:47:15 +0300

    add apps

--- hw/code/ui/locators/mobile_apps_locators.py
+++ hw/code/ui/locators/mobile_apps_locators.py
@@ -1,7 +1,9 @@
 from selenium.webdriver.common.by import By
 
 class MobileAppsLocators:
-    EXAMPLE_INPUT = (
-        By.XPATH,
-        "//input[contains(@class, 'EXAMPLEInput__el')]"
-    )
+    CREATE_BUTTON = (By.XPATH, "//*[contains(@class, 'vkuiButton__content') and text()='Добавить приложение']")
+    INPUT_APP = (By.XPATH,'//input[@placeholder="Введите ссылку на приложение"]')
+    CREATE_APP_BUTTON = (By.XPATH, "//*[contains(@class, 'vkuiButton__content') and text()='Добавить']")
+    MODAL_APP = (By.XPATH, "//div[contains(@class, 'vkuiModalPage') and contains(@class, 'ModalManagerPage_medium__')]")
+    INVALID_MESSAGE = (By.XPATH, "//span[contains(@class, 'vkuiTypography') and contains(text(), 'Введите корректную ссылку на приложение')]" )
+
--- hw/code/ui/pages/mobile_apps_page.py
+++ hw/code/ui/pages/mobile_apps_page.py
@@ -2,10 +2,35 @@ from .page import Page
 from ui.locators.mobile_apps_locators import MobileAppsLocators
 
 class MobileAppsPage(Page):
-    URL = "" # Урл страницы
+    URL = "https://ads.vk.com/hq/apps" # Урл страницы
     locators = MobileAppsLocators()
     # Далее тут можно свои константы
 
-    # И пишем атомарные функции класса, которые мы будем тестировать в другом месте, эти 
+    def click_create_new_app_button(self):
+        """Клик по кнопке создания приложения"""
+        self.click(self.locators.CREATE_BUTTON)
+
+    def get_app_input_field(self):
+        """Получение поля ввода домена"""
+        return self.find(self.locators.INPUT_APP, 10)
+
+    def fill_input_app_field(self, field, text):
+        """Заполнение поля ввода с проверкой"""
+        field.send_keys(text)
+        assert field.get_attribute("value") == text
+
+    def click_app_button(self):
+        """Клик по кнопке создания приложения"""
+        self.scroll_and_click(self.locators.CREATE_APP_BUTTON, 20)
+
+    def get_modal_element(self):
+        """Получение элемента модального окна"""
+        return self.find(self.locators.MODAL_APP, 30)
+
+    def get_error_message(self):
+        """Получение сообщения об ошибке"""
+        return self.find(self.locators.INVALID_MESSAGE)
+
+    # И пишем атомарные функции класса, которые мы будем тестировать в другом месте, эти
     # функции должны быть элементарными и выполнять 1 действие, например клик по кнопке, 
     # нахождение инпута или ввод текст в инпут, это будут все отдельные функции, совмещаются они в /hw/code/TestYourNamePage.py
